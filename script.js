4eccafe244e86fe52a08c49cb843102845908e08
AfzalNormatov <91549976+AfzalNormatov@users.noreply.github.com>
2021-10-29 16:05:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const quoteContainer = document.querySelector('#quote-container');
 const quoteText = document.querySelector('.quote');
 const authorText = document.querySelector('.author');
 const newQuoteBtn = document.querySelector('.new-quote');
+const loader = document.querySelector('.loader');
 
 let apiQuotes = [];
 
+function loading() {
+    loader.hidden =  false;
+    quoteContainer.hidden = true;
+}
+
+function complete() {
+    loader.hidden =  true;
+    quoteContainer.hidden = false;
+}
 
 function newQuote() {
+    loading();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     if(!quote.author){
         authorText.textContent = 'Unknown';
@@ -14,6 +25,7 @@ function newQuote() {
         authorText.textContent = quote.author;
     }
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
@@ -21,7 +33,7 @@ function newQuote() {
 
 
 async function getQuotes() {
-const apiUrl = 'https://type.fit/api/quotes';
+    const apiUrl = 'https://type.fit/api/quotes';
 
     try{
         const response = await fetch(apiUrl);
